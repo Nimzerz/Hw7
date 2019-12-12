8087d53adb1fc183032e1f7eda46b759d755ae52
Nimzerz <aminaimi14@gmail.com>
2019-12-11 19:36:46 -0800

    added test, added comments

--- sort_dependencies/src/DirectedGraph.cpp
+++ sort_dependencies/src/DirectedGraph.cpp
@@ -32,27 +32,31 @@ void DirectedGraph::printGraph() {
 // https://www.geeksforgeeks.org/cpp-program-for-topological-sorting/
 // used this link as a reference for this function
 void DirectedGraph::topologySort(int val, LinkedStack<int> &sorted, set<int> &visited) {
-    visited.insert(val);
-    if (nodes.at(val) == nullptr) {
-        return;
-    }
+    visited.insert(val); // add the node to the set of visited nodes
+
+    // recursively calls this function with all non-visited "neighbors" of this node
     for (SingleLinkedList<int>::Iterator it = nodes.at(val)->begin(); it != nodes.at(val)->end(); it++) {
         if (!visited.count(*it)) {
             topologySort(*it, sorted, visited);
         }
     }
-    sorted.push(val);
+    sorted.push(val); // add this node to the solution
 }
 
 vector<int> *DirectedGraph::topologySort() {
-    set<int> visited;
-    LinkedStack<int> sorted;
+    set<int> visited; // stores the nodes that have been visited
+    LinkedStack<int> sorted; // stores the generated solution to the sort
+
+    // iterates over all entries of the map to ensure every node is visited
     for (map<int, SingleLinkedList<int> *>::iterator it = nodes.begin(); it != nodes.end(); it++) {
         if (!visited.count(it->first)) {
             topologySort(it->first, sorted, visited);
         }
     }
+
     vector<int>* ans = new vector<int>();
+
+    //moves the answer into a vector
     while (!sorted.isEmpty()) {
         ans->push_back(sorted.peek());
         sorted.pop();
--- sort_dependencies/test/DirectedGraphSortTest.cpp
+++ sort_dependencies/test/DirectedGraphSortTest.cpp
@@ -58,3 +58,42 @@ TEST(topology_sort, test_1_nodes) {
     delete v;
 }
 
+TEST(topology_sort, test_example_case) {
+    Edge edges[] =
+            {
+                    // pair (x, y) represents edge from x to y
+                    { 7, 6 }, { 7, 5 }, { 6, 4 },
+                    { 6, 3 }, { 5, 4 }, { 5, 2 },
+                    { 3, 1 }, { 2, 1 }, { 1, 0 }
+            };
+
+    //stores all possible solutions to the topological sort of the graph
+    vector<vector<int>> answers = {{7, 6, 5, 4, 3, 2, 1, 0},
+                                   {7, 6, 5, 4, 2, 3, 1, 0},
+                                   {7, 6, 5, 2, 4, 3, 1, 0},
+                                   {7, 6, 5, 3, 4, 2, 1, 0},
+                                   {7, 6, 5, 2, 3, 4, 1, 0},
+                                   {7, 6, 5, 3, 2, 4, 1, 0},
+                                   {7, 5, 6, 4, 3, 2, 1, 0},
+                                   {7, 5, 6, 4, 2, 3, 1, 0},
+                                   {7, 5, 6, 2, 4, 3, 1, 0},
+                                   {7, 5, 6, 3, 4, 2, 1, 0},
+                                   {7, 5, 6, 2, 3, 4, 1, 0},
+                                   {7, 5, 6, 3, 2, 4, 1, 0}};
+
+    DirectedGraph graph(edges, sizeof(edges)/sizeof(edges[0]));
+    auto v = graph.topologySort();
+    bool solutionFound = false;
+
+    //checks if the answer generated matches a solution
+    for (int i = 0; i < answers.size(); i++) {
+        if (*v == answers[i]) {
+            solutionFound = true;
+            break;
+        }
+    }
+
+    ASSERT_TRUE(solutionFound);
+    delete v;
+}
+
